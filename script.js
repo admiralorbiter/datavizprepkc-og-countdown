241de5cd6581ea9eabcdd25a79b1aa42907fb147
Jon Lane <admiralorbiter@gmail.com>
2022-02-21 21:05:47 -0600

    cleaning up some code

--- script.js
+++ script.js
@@ -1,36 +1,6 @@
-function demo(){
-    var countDownDate = new Date("Feb 25, 2022 01:30:00").getTime();
-
-    // Update the count down every 1 second
-    var x = setInterval(function() {
-
-        // Get today's date and time
-        var now = new Date().getTime();
-
-        // Find the distance between now and the count down date
-        var distance = countDownDate - now;
-
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        // Display the result in the element with id="demo"
-        document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-        + minutes + "m " + seconds + "s ";
-
-        // If the count down is finished, write some text
-        if (distance < 0) {
-            clearInterval(x);
-            document.getElementById("demo").innerHTML = "EXPIRED";
-        }
-    }, 1000);
-}
-
 const year = new Date().getFullYear();
-const fourthOfJuly = new Date(year, 2,3).getTime();
-const fourthOfJulyNextYear = new Date(year + 1, 2, 3).getTime();
+const date = new Date(year, 2,3).getTime();
+const dateNextYear = new Date(year + 1, 2, 3).getTime();
 const month = new Date().getMonth();
 
 // countdown
@@ -42,13 +12,10 @@ let timer = setInterval(function() {
   // get the difference
   let diff;
   if(month > 6) {
-    diff = fourthOfJulyNextYear - today;
+    diff = dateNextYear - today;
   } else {
-    diff = fourthOfJuly - today;
+    diff = date - today;
   }
-
-
-
 
   // math
   let days = Math.floor(diff / (1000 * 60 * 60 * 24));
@@ -58,14 +25,13 @@ let timer = setInterval(function() {
 
   // display
   document.getElementById("timer").innerHTML =
-    "<div class=\"days\"> \
-  <div class=\"numbers\">" + days + "</div>days</div> \
-<div class=\"hours\"> \
-  <div class=\"numbers\">" + hours + "</div>hours</div> \
-<div class=\"minutes\"> \
-  <div class=\"numbers\">" + minutes + "</div>minutes</div> \
-<div class=\"seconds\"> \
-  <div class=\"numbers\">" + seconds + "</div>seconds</div> \
-</div>";
-
+    " <div class=\"days\"> \
+      <div class=\"numbers\">" + days + "</div>days</div> \
+      <div class=\"hours\"> \
+      <div class=\"numbers\">" + hours + "</div>hours</div> \
+      <div class=\"minutes\"> \
+      <div class=\"numbers\">" + minutes + "</div>minutes</div> \
+      <div class=\"seconds\"> \
+      <div class=\"numbers\">" + seconds + "</div>seconds</div> \
+      </div>";
 }, 1000);
